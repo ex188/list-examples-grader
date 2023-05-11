9e9dbd510fa610513a3e5e6ae825c89881a07b93
ex188 <594718333@qq.com>
2023-05-10 18:29:55 -0700

    fgh

--- grade.sh
+++ grade.sh
@@ -9,6 +9,22 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 
+if [[ -f "student-submission/ListExamples.java" ]]
+then    
+    echo 'You submitted the correct file'
+else    
+    echo 'File not detected'
+    exit 1
+fi 
+
+cp -r student-submission/ListExamples.java grading-area
+
+javac student-submission/ListExamples.java TestListExamples.java 2>err.txt
+cat err.txt
+exit 1
+
+
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
